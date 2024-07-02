28b56c257911140c5e44888fba49d6d05e19b077
rifkimaulana <rifkimaulana@localhost.localdomain>
2024-07-03 01:51:07 +0700

    Fix login form validation bug

--- config/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const router = express.Router()
-import controller from "../controllers/index.js"
-import auth from "../middleware/auth.js"
-
-router.get("/", auth, controller.Post.getAllPosts)
-
-export default router
